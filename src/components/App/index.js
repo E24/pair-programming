5262ba6c806b825c66f5a8a289801ecb8ab7b47a
Fredrik Stave <fredrik.stave@schibsted.com>
2021-06-21 14:01:33 +0200

    feat: added inital version of the project

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid">
+      <div className="box col5">
+        {/* You're code goes here */}
+      </div>
+      <div className="box col7">
+        <div className="netboard">ADVERT</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
